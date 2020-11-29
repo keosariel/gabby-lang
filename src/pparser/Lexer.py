f04dfa4cc4daf7cf8280fe2d4f2badc6ac6a628d
kenneth gabriel <31022243+keosariel@users.noreply.github.com>
2020-11-29 15:20:10 +0100

    Add files via upload

---
+++ src/pparser/Lexer.py
@@ -0,0 +1,106 @@
+from sly import Lexer
+
+def group(*choices): return '(' + '|'.join(choices) + ')'
+def any(*choices): return group(*choices) + '*'
+def maybe(*choices): return group(*choices) + '?'
+
+Hexnumber = r'0[xX](?:_?[0-9a-fA-F])+'
+Binnumber = r'0[bB](?:_?[01])+'
+Octnumber = r'0[oO](?:_?[0-7])+'
+Decnumber = r'(?:0(?:_?0)*|[1-9](?:_?[0-9])*)'
+Exponent = r'[eE][-+]?[0-9](?:_?[0-9])*'
+Pointfloat = group(r'[0-9](?:_?[0-9])*\.(?:[0-9](?:_?[0-9])*)?',
+                   r'\.[0-9](?:_?[0-9])*') + maybe(Exponent)
+Expfloat = r'[0-9](?:_?[0-9])*' + Exponent
+
+class PLexer(Lexer):
+    tokens = {
+        NAME, 
+        NUMBER, 
+        STRING, 
+        FLOAT, 
+        PLUS, 
+        MINUS,
+        DIVIDE,
+        LPAREN, 
+        RPAREN, 
+        LBRACE, 
+        RBRACE, 
+        LT, 
+        LE, 
+        GT, 
+        GE,
+        EQ, 
+        EQEQ, 
+        NE, 
+        IF, 
+        ELSE, 
+        DEF, 
+        RETURN,
+        TIMES,
+        COLON,
+        MOD,
+        WHILE, 
+        UNTIL, 
+        BREAK, 
+        CONTINUE,
+        AND, 
+        OR, 
+        COMMA
+    }
+
+    literals = {',',';'}
+
+    FLOAT = group(Pointfloat, Expfloat)
+    
+    NUMBER = group(
+                Hexnumber, 
+                Binnumber, 
+                Octnumber, 
+                Decnumber
+            )
+
+    ignore = ' \t\r'
+    @_(r'\n')
+    def newline(self,t ):
+        self.lineno += 1
+
+
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    NAME['if'] = IF
+    NAME['else'] = ELSE
+    NAME['def'] = DEF
+    NAME['return'] = RETURN
+    NAME['while'] = WHILE
+    NAME['until'] = UNTIL
+    NAME['break'] = BREAK
+    NAME['continue'] = CONTINUE
+    NAME['and'] = AND
+    NAME['or'] = OR
+    STRING = r'(\".*?\")|(\'.*?\')'
+    GE = r'>='
+    GT = r'>'
+    LE = r'<='
+    LT = r'<'
+    NE = r'!='
+    EQEQ = r'=='
+    EQ = r'='
+    LBRACE = r'\{'
+    RBRACE = r'\}'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    PLUS = r'\+'
+    MINUS = r'-'
+    TIMES = r'\*'
+    DIVIDE = r'/'
+    MOD = r'%'
+    COLON = r':'
+    COMMA = r','
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    def error(self, t):
+        print(f'Illegal character {t.value[0]}, in line {self.lineno}, index {self.index}')
+        exit()
--- src/pparser/Parser.py
+++ src/pparser/Parser.py
@@ -1,1 +1,146 @@
+from src.pparser.Lexer import PLexer
+import src.pparser.utils as utils
+from sly import Parser
+import pprint
 
+class PParser(Parser):
+    tokens = PLexer.tokens
+
+    precedence = (
+        ('nonassoc', NE, LT, LE, GT, GE,EQEQ),
+        ('left', PLUS, MINUS),
+        ('left', TIMES, DIVIDE)
+    )
+
+    def __init__(self):
+        self.ast = ('Module',{'body':[]})
+    
+    @_("statements")
+    def body(self, p):
+        self.ast[1]['body'] = p.statements
+
+    
+    @_('statement')
+    def statements(self, p):
+        return [p.statement]
+
+    @_('statements statement')
+    def statements(self, p):
+        p.statements.append(p.statement)
+        return p.statements
+    
+    @_('RETURN expr')
+    def statement(self,p):
+        return ('Return',{'value':p.expr})
+    
+        
+    @_('DEF NAME LPAREN def_params RPAREN COLON NAME LBRACE statements RBRACE')
+    def statement(self,p):
+        return utils.function(p.NAME0,p.def_params,p.NAME1,p.statements)
+    
+    @_('IF expr LBRACE statements RBRACE')
+    def statement(self,p):
+        return utils.if_stmt(p.statements,orelse=[],test=p.expr)
+    
+    @_('IF expr LBRACE statements RBRACE ELSE LBRACE statements RBRACE')
+    def statement(self,p):
+        return utils.if_stmt(p.statements0,p.statements1,p.expr)
+    
+    @_('WHILE expr LBRACE statements RBRACE')
+    def statement(self,p):
+        return utils.while_block(p.statements,p.expr)
+    
+    @_('UNTIL expr LBRACE statements RBRACE')
+    def statement(self,p):
+        return utils.until_block(p.statements,p.expr)
+    
+    @_('NAME EQ expr')
+    def statement(self,p):
+        return utils.var_assign(p.NAME,p.expr)
+    
+    @_('NAME LPAREN params RPAREN')
+    def statement(self,p):
+        return utils.func_call(p.NAME,p.params)
+    
+    @_('def_params COMMA def_param')
+    def def_params(self,p):
+        p.def_params.append(p.def_param)
+        return p.def_params
+    
+    @_('def_param')
+    def def_params(self,p):
+        return [p.def_param]
+    
+    @_('NAME COLON NAME')
+    def def_param(self,p):
+        return {'name':p.NAME0,'type':p.NAME1}
+    
+    @_('')
+    def def_param(self,p):
+        return
+    
+    @_('params COMMA param')
+    def params(self,p):
+        p.params.append(p.param)
+        return p.params
+    
+    @_('param')
+    def params(self,p):
+        return [p.param]
+    
+    @_('expr')
+    def param(self,p):
+        return p.expr
+    
+    @_('')
+    def param(self,p):
+        return
+    
+    @_('NAME LPAREN params RPAREN')
+    def expr(self,p):
+        return utils.func_call(p.NAME,p.params)
+    
+    @_('expr PLUS expr',
+       'expr MINUS expr',
+       'expr TIMES expr',
+       'expr DIVIDE expr',
+       'expr MOD expr',
+       'expr GT expr',
+       'expr GE expr',
+       'expr LT expr',
+       'expr LE expr',
+       'expr NE expr',
+       'expr EQEQ expr',
+       'expr AND expr',
+       'expr OR expr')
+    def expr(self,p):
+        return utils.expression(p[1],p.expr0,p.expr1)
+    
+    
+    @_('LPAREN expr RPAREN')
+    def expr(self,p):
+        return p.expr
+
+    @_('NAME')
+    def expr(self,p):
+        return utils.data('Name',p.NAME)
+    
+    @_('NUMBER')
+    def expr(self,p):
+        return utils.data('Number',int(p.NUMBER))
+    
+    @_('MINUS NUMBER')
+    def expr(self,p):
+        return utils.data('Number',int(p.NUMBER)*-1)
+    
+    @_('FLOAT')
+    def expr(self,p):
+        return utils.data('Float',float(p.FLOAT))
+        
+    @_('MINUS FLOAT')
+    def expr(self,p):
+        return utils.data('Float',float(p.FLOAT)*-1)
+    
+    @_('STRING')
+    def expr(self,p):
+        return utils.data('String',p.STRING)
